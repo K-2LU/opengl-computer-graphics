3d03faa5735eab0afe7a34e63dc40048129d6626
K2LU <rigansust@gmail.com>
2025-05-19 17:07:02 +0600

    basic.c

--- codes/01-basic.c
+++ codes/01-basic.c
@@ -2,11 +2,42 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-void display()  {}
+void display()  {
+    glClear(GL_COLOR_BUFFER_BIT);                   // clear any previous
+    glLoadIdentity();
+
+    // draw stuff
+    glPointSize(5.0);
+    glBegin(GL_POINTS);
+    int initX, initY;
+    initX = 0;
+    initY = 10;
+    for (int i = 0; i < 5; i++) {
+        glVertex2f(initX, initY);
+        glVertex2f(-initX, initY);
+        initX += 2;
+        initY -= 2;
+    }   
+    glEnd();
+
+    glFlush();
+}
+
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 1.0);               // R G B
+}
+
+void reshape(int w, int h)  {
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(-10, 10, -10, 10);
+    glMatrixMode(GL_MODELVIEW);
+}
 
 int main(int argc, char **argv) {
+    
     glutInit(&argc, argv);
-    // glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutInitDisplayMode(GLUT_RGB);
 
     glutInitWindowPosition(200, 100);               // specify window position
@@ -14,6 +45,7 @@ int main(int argc, char **argv) {
 
     glutCreateWindow("Demo");                       // create the window
     glutDisplayFunc(display);
-    
+    glutReshapeFunc(reshape);
+    init();
     glutMainLoop();
 }
